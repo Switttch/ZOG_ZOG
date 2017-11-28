ae34f98a39f83e78911ed26c9d205cfa42e7587e
Switttch <34029473+Switttch@users.noreply.github.com>
2017-11-28 12:35:16 +1100

    CHANGE1
    FORK_newBranch

--- 02_01.py
+++ 02_01.py
@@ -1,15 +1,9 @@
-
 # coding: utf-8
 
 # ZOG_ZOG2
 
-
 get_ipython().system(' pip install Seaborn')
-
-
 # In[2]:
-
-
 import numpy as np
 from numpy.random import randn
 import pandas as pd
@@ -19,10 +13,8 @@ import matplotlib.pyplot as plt
 from matplotlib import rcParams
 import seaborn as sb
 
-
 # In[3]:
 
-
 get_ipython().magic('matplotlib inline')
-rcParams['figure.figsize'] = 5, 4
-sb.set_style('whitegrid')
+ABC=100
+Print ('ABC')
